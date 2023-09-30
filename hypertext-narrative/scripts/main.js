3e179f1f0adbb4dcc2087defd0f19ceb68e30dff
550tealeaves <nastasia.claire89@gmail.com>
2023-09-30 19:22:47 -0400

    was able to get the biography to scroll left
    Ideally want it to scroll top to bottom

--- hypertext-narrative/scripts/main.js
+++ hypertext-narrative/scripts/main.js
@@ -1,1 +1,12 @@
 console.log('blah');
+
+//was trying to create JS scrolling text but not working
+var style = document.createElement('style');
+var position = 'right';
+
+style.innerHTML = `@keyframes my-animation{
+    0%{${position}: -${document.querySelector('.moving-parts', 'a').offsetWidth + 10}px;}
+    100%{${position}: 100%;}
+}`;
+
+document.head.append(style);
