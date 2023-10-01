5ea0036affb52715e1b1700e6bc30eaea92a2b50
550tealeaves <nastasia.claire89@gmail.com>
2023-10-01 11:21:11 -0400

    commented out jquery code

--- hypertext-narrative/scripts/main.js
+++ hypertext-narrative/scripts/main.js
@@ -12,33 +12,33 @@ style.innerHTML = `@keyframes my-animation{
 document.head.append(style);
 
 
-//SCROLLYTELLING - https://pudding.cool/process/how-to-implement-scrollytelling/
-var handleItemFocus = function (event, item) {
-    var step = item.data.step
-    graphic.update(step)
-}
+// //SCROLLYTELLING - https://pudding.cool/process/how-to-implement-scrollytelling/
+// var handleItemFocus = function (event, item) {
+//     var step = item.data.step
+//     graphic.update(step)
+// }
 
-var handleContainerScroll = function (event) {
-    var bottom = false
-    var fixed = false
+// var handleContainerScroll = function (event) {
+//     var bottom = false
+//     var fixed = false
 
-    var bb = $graphicEl[0].getBoundingClientRect()
-    var bottomFromTop = bb.bottom - viewportHeight
+//     var bb = $graphicEl[0].getBoundingClientRect()
+//     var bottomFromTop = bb.bottom - viewportHeight
 
-    if (bb.top < 0 && bottomFromTop > 0) {
-        bottom = false
-        fixed = true
-    } else if (bb.top < 0 && bottomFromTop < 0) {
-        bottom = true
-        fixed = false
-    }
+//     if (bb.top < 0 && bottomFromTop > 0) {
+//         bottom = false
+//         fixed = true
+//     } else if (bb.top < 0 && bottomFromTop < 0) {
+//         bottom = true
+//         fixed = false
+//     }
 
-    toggle(fixed, bottom)
-}
+//     toggle(fixed, bottom)
+// }
 
-$graphicEl.scrollStory({
-    contentSelector: '.trigger',
-    triggerOffset: halfViewportHeight,
-    itemfocus: handleItemFocus,
-    containerscroll: handleContainerScroll,
-})
+// $graphicEl.scrollStory({
+//     contentSelector: '.trigger',
+//     triggerOffset: halfViewportHeight,
+//     itemfocus: handleItemFocus,
+//     containerscroll: handleContainerScroll,
+// })
