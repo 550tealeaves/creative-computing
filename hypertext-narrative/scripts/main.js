beee7a39588d695da418966afc7d0a51d8cf59f3
550tealeaves <nastasia.claire89@gmail.com>
2023-10-01 08:08:27 -0400

    trying to add JQuery scrollytelling code

--- hypertext-narrative/scripts/main.js
+++ hypertext-narrative/scripts/main.js
@@ -10,3 +10,35 @@ style.innerHTML = `@keyframes my-animation{
 }`;
 
 document.head.append(style);
+
+
+//SCROLLYTELLING - https://pudding.cool/process/how-to-implement-scrollytelling/
+var handleItemFocus = function (event, item) {
+    var step = item.data.step
+    graphic.update(step)
+}
+
+var handleContainerScroll = function (event) {
+    var bottom = false
+    var fixed = false
+
+    var bb = $graphicEl[0].getBoundingClientRect()
+    var bottomFromTop = bb.bottom - viewportHeight
+
+    if (bb.top < 0 && bottomFromTop > 0) {
+        bottom = false
+        fixed = true
+    } else if (bb.top < 0 && bottomFromTop < 0) {
+        bottom = true
+        fixed = false
+    }
+
+    toggle(fixed, bottom)
+}
+
+$graphicEl.scrollStory({
+    contentSelector: '.trigger',
+    triggerOffset: halfViewportHeight,
+    itemfocus: handleItemFocus,
+    containerscroll: handleContainerScroll,
+})
