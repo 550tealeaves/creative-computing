bcaa3bc1dd571bebf357bbaa1ad769aa1ce2ddcf
550tealeaves <nastasia.claire89@gmail.com>
2023-10-04 22:42:19 -0400

    got the event listener to work

--- event-listener/scripts/main.js
+++ event-listener/scripts/main.js
@@ -1,5 +1,16 @@
-newdiv = document.createElement('div');
-document.querySelector('body').appendChild(newdiv);
-console.log('HI IM WORKING');
+//Select the element you want to add event listener to 
+let btn = document.querySelector('.btn');
+//Add eventListener - 
+//(1) define the type of event - ie clicked
+//(2) Define function and pass in the parameters
+btn.addEventListener('mouseover', function () {
+    let showMe = document.querySelector('.show-me');
+    showMe.style.opacity = '1';
+    showMe.scrollTo['font-size'] = '190px';
+    showMe.style.transform = 'skew(30deg)';
+})
+window.addEventListener('scroll', function () {
+    console.log("yes, I'm scrolling");
+})
 
-const obj = { name: 'omar' };
+
