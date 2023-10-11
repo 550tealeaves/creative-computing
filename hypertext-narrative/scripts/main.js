4b258748202d40553b7a9c295eddd3b3d290071b
550tealeaves <nastasia.claire89@gmail.com>
2023-10-11 00:15:42 -0400

    changed transition speed and width and other style changes

--- hypertext-narrative/scripts/main.js
+++ hypertext-narrative/scripts/main.js
@@ -29,7 +29,7 @@ document.addEventListener('DOMContentLoaded', async function () {
         .onStepExit((response) => {
             //detect if it's the last step
             if (response.index == numberSteps - 1) {
-                changeBallSize(1500, 1000); //this changes the size
+                changeBallSize(900, 1000); //this changes the size
             }
         });
 });
