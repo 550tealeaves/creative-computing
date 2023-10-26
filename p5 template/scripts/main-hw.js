f522f1407e098a3f9e20de4a63c6966eca371d0f
550tealeaves <nastasia.claire89@gmail.com>
2023-10-25 20:13:47 -0400

    added more p5 notes

--- p5 template/scripts/main-hw.js
+++ p5 template/scripts/main-hw.js
@@ -1,12 +1,12 @@
-function setup() {
-    createCanvas(500, 500)
-    background(100);
-}
+// function setup() {
+//     createCanvas(500, 500)
+//     background(100);
+// }
 
-function draw() {
-    ellipse(x, height / 2, 20, 20);
-    x = x + 1;
-}
+// function draw() {
+//     ellipse(x, height / 2, 20, 20);
+//     x = x + 1;
+// }
 
 
 //MOUSE INTERACTIVITY
@@ -75,14 +75,72 @@ function draw() {
 // }
 
 //FRAME BY FRAME MOVEMENT
-let x = 0;
+// let x = 0;
 
+// function setup() {
+//     createCanvas(500, 500)
+//     background(100);
+// }
+
+// function draw() {
+//     ellipse(x, height / 2, 20, 20);
+//     x = x + 1;
+// }
+
+
+//RANDOMNESS
+// function setup() {
+//     createCanvas(500, 500)
+//     background(300);
+// }
+
+// function draw() {
+//     console.log(min(random(1, 40), random(1, 40)))
+// }
+
+
+
+//SKYSCRAPER WITH RANDOMNESS
 function setup() {
-    createCanvas(500, 500)
-    background(100);
+    createCanvas(400, 400);
 }
 
 function draw() {
-    ellipse(x, height / 2, 20, 20);
-    x = x + 1;
+    background(255);
+    noLoop();
+
+    let numRects = 40;
+    let rectWidth = width / numRects;
+    fill('blue');
+    noStroke();
+    //start at #, end at another & increment by certain amount
+    //it's going to the width of the page - and incrementing by the rectangle width so there are no overlaps
+    //start at x (0) and keep going until reaching the width & keep incrementing by the width of the rectangle
+    //This starts at 0, & rect width = 10, so 0, 10, 20, 30 etc 390 - stops at 390 b/c < NOT <=
+    for (x = 0; x < width; x += rectWidth) {
+        //let rectHeight = random(1, 150);
+        let rectHeight = max(random(1, 150), random(1, 150)); //this line makes the buildings look taller
+        rect(x, height / 2, rectWidth, -rectHeight)
+    }
 }
+
+
+//ANOTHER WAY TO CREATE SHAPE WITH FOR LOOP
+// function setup() {
+//     createCanvas(400, 400);
+//     fill(160, 200, 23);
+// }
+
+// function draw() {
+//     background(255);
+//     noLoop();
+
+//     let numSquares = 10;
+//     let squareSize = width / numSquares;
+
+//     //start at 0, and go till the numSquares (10) and increment by 1 so will stop at 9
+//     for (x = 0; x < numSquares; x += 1) {
+//         rect(50, 50, squareSize);
+//         console.log(x); //show 0-9
+//     }
+// }
