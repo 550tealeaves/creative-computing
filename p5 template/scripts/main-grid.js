78ad22f6845c210bf0c23d9b00071118020dbde4
550tealeaves <nastasia.claire89@gmail.com>
2023-10-30 12:02:19 -0400

    created circle grid for hw 9

--- p5 template/scripts/main-grid.js
+++ p5 template/scripts/main-grid.js
@@ -1,30 +1,25 @@
+//week 9 hw assignment - https://editor.p5js.org/claire89/sketches/zgnRCFHOW
+
 function setup() {
     createCanvas(400, 400);
 }
 
 function draw() {
-    background(220);    
+    background(240);    
 
-    let numrows = 1000;
-    let numcols = 1000;
-    let paddingFactor = 0;
-    let xPadding = paddingFactor / numrows;
-    let yPadding = paddingFactor / numcols;
-
-
-
-    //For (var BEGIN; END; INTERVAL){
-    //DO SOMETHING }
-    for (var x = 0; x < width; x += width / 10) {
-        for (var y = 0; y < height; y += height / 5) {
-            stroke(255);
-            strokeWeight(1);
-            line(x, 0, x, height);
-            line(0, y, width, y);
-            noStroke();
-            fill(60, 59, 251);
-            circle(200, 200, 50); //circle appears in the middle
-
+    //1st for loop (x=0), x<width - changes the circles on the vertical lines (col)
+    //2nd for loop (y-0), y<height - changes circles on horizontal lines (rows)
+    //increased the last # in each for loop statement (width/#), (height/#) to increase the circles - originally started at 10
+    for (var x = 0; x < width; x += width / 25) { 
+        for (var y = 0; y < height; y += height / 25) {
+            noStroke(); //no outline 
+            fill(60, 59, 251); //color from assignment
+            circle(x, y, 8); 
+            //below code would create white grid lines intersecting the circles
+            // stroke(255);
+            // strokeWeight(0.1);
+            // line(x, 0, x, height);
+            // line(0, y, width, y);
         }
     }
 }
