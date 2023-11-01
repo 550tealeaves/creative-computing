82f801e6099f1d5c430c56d6c8d191d83bcce415
550tealeaves <nastasia.claire89@gmail.com>
2023-11-01 01:43:49 -0400

    removed unused code

--- p5 template/scripts/main-board-static.js
+++ p5 template/scripts/main-board-static.js
@@ -4,12 +4,25 @@
 //text(string, x, y, [maxWidth], [maxHeight]); (w/h optional)
 
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(400, 400); //this creates 8x8 board which is good for checkers
+    //createCanvas(600, 400); 
     background('#c700ff');
 }
 
 function draw() {
     drawGrid();
+
+    // fill(150, 83, 222);
+    // circle(225, 225, 49);
+
+    // fill('orange');
+    // circle(325, 125, 49);
+
+    // fill('#d4cf55');
+    // circle(375, 475, 49);
+
+    // fill(63, 161, 146);
+    // circle(125, 325, 49);
 }
 
 function drawGrid() {
@@ -18,14 +31,16 @@ function drawGrid() {
     for (var i = 0; i < 12; i++) {
         for (var j = 0; j < 10; j++) {
             line(i * 50, 0, i * 50, 500); //adjusts vertical lines
+            fill(255);
+            text(i * 50, (i * 50) + 2, 10); //1st # increments #s on top row by 50 (0*50, 1*50, 2*50 etc), 2nd # = (x) - places each text at (0*50) + 2 (x=2, x=52, x-152 - moves #s to the right of the line). This done to prevent 2 (0's) from showing, 3rd # = (y) moves #s up/down y-axis
             line(0, j * 50, 600, j * 50); //adjusts horizontal lines
-            
+            fill(255);
+            text(j * 50, 2, (j * 50) + 10); //1st increments #s on column by 50 (0*50, 1*50 etc), 2nd # (x) lists values at x=2, 3rd # (y) moves #s down y-axis (y=10, y=60, y=110) - moves #s down & under the lines
+            fill(0);
             //CREATING THE BOARD GAME - squares have to be 100 spaces apart both horizontally & vertically
             //alternating rows start at 0 and space out every 100 (i*100)
             //alternating rows start at 50 & space out every 100 (i*100) + 50
             //y changes
-
-            fill(0);
             square(0, (i * 100), 50); //first column of black squares
             square((i * 100), 0, 50); //top row of black squares
             square((i * 100) + 50, 50, 50); //2nd row of black squares
@@ -38,16 +53,18 @@ function drawGrid() {
             square((i * 100), 400, 50)//9th row of black squares
             square((i * 100) + 50, 450, 50);//10th row of black squares
 
-            //CREATE AND POSITION 1ST SET OF "CHECKER" PIECES
+            //CREATE AND POSITION THE "CHECKER" PIECES
             fill('red');
             circle((i * 100) + 25, 25, 49);//created 1st row of circles
             circle((i * 100) + 75, 75, 49);//created 2nd row of circles
-            circle(random(i * 100) - 75, 125, 49);//created 3rd row of circles
-            //CREATE AND POSITION 2ND SET OF "CHECKER" PIECES
+            circle((i * 100) - 75, 125, 49);//created 3rd row of circles
+            //circle((i * 100) + 75, 175, 49);
             fill('seagreen');
-            circle(random(i * 100) + 75, 275, 49);
+            circle((i * 100) + 75, 275, 49);
             circle((i * 100) - 275, 325, 49); //creates 2nd row of other pieces
             circle((i * 100) + 75, 375, 49);
+            // circle((i * 100) - 75, 425, 49); //not needed b/c of reduced canvas size
+            // circle((i * 100) + 75, 475, 49); //not needed b/c of reduced canvas size
         }
     }
 }
