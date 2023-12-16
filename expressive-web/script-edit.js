4579d6d532612eb68ca0f4c3255f7e973ce4c6d0
550tealeaves <nastasia.claire89@gmail.com>
2023-12-16 18:33:33 -0500

    found code that would help me move the train image
    https://chat.openai.com/c/b1325e01-ba50-4316-b0e5-e1de3c705606
    
    <style>
            #movingImage {
                position: absolute;
            }
        </style>
    </head>
    <body>
        <img id="movingImage" src="images/179th street.jpg" alt="Moving Image">
    
        <script>
            // Get the image element
                var image = document.getElementById('movingImage');
    
                // Set initial position
                var initialX = 0;
                var initialY = 0;
    
                // Set the interval for animation
                var interval = setInterval(moveImage, 7);
    
                function moveImage() {
                    // Update position
                    initialX += 6; // Change this value to control horizontal movement
                    initialY += 2; // Change this value to control vertical movement
    
                    // Apply new position
                    image.style.left = initialX + 'px';
                    image.style.top = initialY + 'px';
    
                    //Optional: Stop the animation after a certain condition (e.g., reached a specific position)
                    if (initialX >= 400) {
                      clearInterval(interval);
                    }
                }
        </script>

--- expressive-web/script-edit.js
+++ expressive-web/script-edit.js
@@ -58,28 +58,87 @@ changeText.style.position = 'relative';
 });
 
 //MOVE THE TRAIN ONTO THE TRACK
-let moveTrain = document.querySelector('.ball');
-moveTrain.addEventListener('click', function(){
-    // moveTrain.style.transform = 'rotate(20deg)';
+// let moveTrain = document.querySelector('.ball');
+// moveTrain.addEventListener('click', function(){
+//     // moveTrain.style.transform = 'rotate(20deg)';
+//     moveTrain.style.position = 'relative';
+//     moveTrain.style.right= '165%';
+// });
+
+//Used this https://stackoverflow.com/questions/51254897/javascript-move-image-across-page-on-click-and-reset-image-to-original-location
+var moveTrain = null;
+var animate;
+
+function init() {
+    moveTrain = document.getElementById('ball');
     moveTrain.style.position = 'relative';
-    moveTrain.style.right= '165%';
-});
-
-
-//STOP AUDIO FROM PLAYING ON LOAD
-// window.ismuted = true; //doesn't work
-
-
-document.onload = function () {
-    var audios = document.getElementById('embed');
-
-    for (var i = 0; i < audios.length; i++) {
-        audios[i].muted = true;
-    }
+    moveTrain.style.top = '400px';
 }
 
 
+shiftTrain = moveTrain;
+shiftTrain.addEventListener('click', function (){
+    function moveTop(target) {
+        moveTrain.style.top = parseInt(moveTrain.style.top) - 10 + 'px';
+    if (parseInt(moveTrain.style.top) < target) {
+        moveTrain.style.top = target + 'px';
+    }
 
+    if (parseInt(moveTrain.style.top) - 10 > target) {
+        animate = setTimeout(function () { moveTop(target); }, 20);
+    }
+    else {
+        //add the wait here, currently 5 seconds
+        animate = setTimeout(function () { moveBack(400); }, 5000);
+    }
+    }
+
+    function moveBack(target) {
+        moveTrain.style.top = parseInt(moveTrain.style.top) + 10 + 'px';
+    if (parseInt(moveTrain.style.top) > target) {
+        moveTrain.style.top = target + 'px';
+    }
+
+    if (parseInt(moveTrain.style.top) + 10 < target) {
+        animate = setTimeout(function () { moveBack(400); }, 20);
+    }
+    }
+
+})
+
+
+
+//function stop(){
+//clearTimeout(animate);
+//imgObj.style.top = '0px'; 
+//}
+
+window.onload = init;
+
+
+
+
+
+
+
+
+
+//STOP AUDIO FROM PLAYING ON LOAD
+// window.ismuted = true; //ATTEMPT #1 DOESN'T WORK
+
+
+
+//ATTEMPT #2 DOESN'T WORK
+// document.onload = function () {
+//     var audios = document.getElementById('embed');
+
+//     for (var i = 0; i < audios.length; i++) {
+//         audios[i].muted = true;
+//     }
+// }
+
+
+//ATTEMPT #3 BELOW DOES NOT WORK
 // let audio = document.getElementsByTagName('embed');
 // if (ismuted === true) {
 //     audio.muted = false;
@@ -102,6 +161,31 @@ document.onload = function () {
 //     audio.sound = sound;
 //     audio.play();
 // }
+
+
+var sound = true;
+
+function muter() {
+    if (sound === true) {
+        $("embed").prop('muted', true);
+        sound = false;
+        $('button').text('Sound');
+    } else {
+        $("embed").prop('muted', false);
+        sound = true;
+        $('button').text('Mute');
+    }
+}
+
+$('button').click(function () {
+    muter();
+});
+
+//muter.reset();
+
+
+
+
 
 
 //TRY TO GET THE SCROLLY SECTION AUTOMATICALLY MOVING UP WHEN PAGE LOADS
