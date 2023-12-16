1ba42bbdc79b24075760c84374747ff42cd09976
550tealeaves <nastasia.claire89@gmail.com>
2023-12-16 09:10:08 -0500

    got an event listener to work

--- expressive-web/script-edit.js
+++ expressive-web/script-edit.js
@@ -36,9 +36,10 @@ document.addEventListener('DOMContentLoaded', async function () {
 
 
 let openStatement = document.querySelector('.intro');
-openStatement.addEventListener('load', function(){
+openStatement.addEventListener('click', function(){
     let start = document.querySelector('.intro');
     start.style.transition = 'transform 0.2s ease-in-out';
     start.style.position = 'absolute';
     start.style.position.top = '10%';
+    start.style.color = 'red';
 });
