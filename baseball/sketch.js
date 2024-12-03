9aed05230abb0487cbb8628cb1a56f343497c00c
550tealeaves <nastasia.claire89@gmail.com>
2024-12-02 21:48:13 -0500

    placed jackie in the batter's box

--- baseball/sketch.js
+++ baseball/sketch.js
@@ -34,7 +34,7 @@ function draw() {
 
   image(img, x - w / 2, y - h / 2, w, h);
   image(player, mouseX-50, mouseY-50, 100, 100); //moves player image based on cursor position mouseX-50 (moves cursor to right and puts it on img, mouseY-50 moves cursor down img, if no #s, then default cursor is up and to left of img) - last 2 numbers adjusts size (w,h) - https://editor.p5js.org/mellerbr/sketches/Jhz5uwM5
-  image(jackie, x - 95, y + 150, 100, 100);
+  image(jackie, x - 130, y + 130, 100, 100);
 }
 
 function mouseDragged() {
